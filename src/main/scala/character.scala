5f7697fc10fb22dac4f3a2fe4d3075d2514406c7
Antony Angeli <antony.angeli@Antonys-Laptop.local>
2024-07-12 12:23:15 +0100

    add questions

--- src/main/scala/character.scala
+++ src/main/scala/character.scala
@@ -29,4 +29,23 @@ object GuessWho extends App {
 //    characters(Random.nextInt(characters.length))
 //  }
 
+  val questions: List[String] = List(
+    "1. Is their hair colour black?",
+    "2. Is their hair colour brown?",
+    "3. Is their hair colour blonde?",
+    "4. Do they have brown eyes?",
+    "5. Do they have blue eyes?",
+    "6. Do they wear glasses?",
+    "7. Do they wear earrings?",
+    "8. Are they male?",
+    "9. Are they female?",
+    "10. Do they have facial hair?",
+  )
+
+
+  val questionsMap: Map = (
+    1 -> character.haircolourcheck,
+    6 -> character.hasglasses(),
+  )
+
 }
